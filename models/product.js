2ad163c0668b093ac20375c5a65954c269eefb1d
Temitayo Jimoh <temi.boy@MacBook-Pro-3.local>
2021-06-27 10:37:06 -0700

    added api routes

--- models/product.js
+++ models/product.js
@@ -1,1 +1,55 @@
-js
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+//import our database connection from config.js
+const sequelize = require('../config/connection';
+
+// Initialize Product model (table) by entending off Sequelize Model class
+class Product extends Model { }
+
+// set up fields and rules for Product model
+Product.init(
+    {
+        id: {
+            type: DataTypes.INTERGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+
+        },
+        product_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        price: {
+            type: DataTypes.DECIMAL,
+            allowNull: false,
+            validate: {
+                isDecimal: true
+            }
+        },
+        stock: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 10,
+            Validate: {
+                isNumeric: true
+            }
+        },
+        category_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'category',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'product'
+    }
+);
+
+module.exports = Product;
